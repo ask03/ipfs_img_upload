e6ac1522c0448a8646df0cf440f17a5caa1e0731
Allan Kim <alkim03@gmail.com>
2019-02-17 22:56:09 +0900

    uploads img to ipfs and hash to blockchain

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import ipfs from "./ipfs";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null, buffer: null,
+  state = { web3: null, accounts: null, contract: null, buffer: null,
   ipfsHash: "" };
 
   constructor(props) {
@@ -30,10 +30,10 @@ class App extends Component {
         SimpleStorageContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-
+      const ipfsHash = await instance.methods.get().call({from:accounts[0]});
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance, ipfsHash });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -41,19 +41,6 @@ class App extends Component {
       );
       console.error(error);
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
   };
 
   captureFile(event) {
@@ -70,37 +57,42 @@ class App extends Component {
 
   onSubmit(event) {
     event.preventDefault()
+    const {accounts, contract} = this.state;
     console.log('on submit...');
+
     ipfs.files.add(this.state.buffer, (err, result) => {
       if(err) {
         console.log(err)
         return
       }
-
-      this.setState({ ipfsHash: result[0].hash })
-      console.log('ipfsHash', this.state.ipfsHash)
+      contract.methods.set(result[0].hash).send({from:accounts[0]}).then((r) => {
+        console.log(r);
+        this.setState({ipfsHash: result[0].hash})
+        console.log('ipfsHash', this.state.ipfsHash);
+      })
     })
+
   }
 
   render() {
     // if (!this.state.web3) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
-    return (
-      <div className="App">
+      //   return <div>Loading Web3, accounts, and contract...</div>;
+      // }
+      return (
+        <div className="App">
         <h1>Your Image</h1>
         <p>This image is stored on IPFS and The Ethereum Blockchain!</p>
         <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
         <h2>Upload Image</h2>
         <form onSubmit={this.onSubmit}>
-          <input type="file" onChange={this.captureFile} />
-          <input type="submit" />
+        <input type="file" onChange={this.captureFile} />
+        <input type="submit" />
         </form>
 
-      </div>
-    );
+        </div>
+      );
+    }
+
   }
 
-}
-
-export default App;
+  export default App;
--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,15 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
+  it("...should store the hash QmQo6GX8jpHxfJoMomNHnVwGxaEUXx3eRC7jTkPVzX5vwh.", async () => {
     const simpleStorageInstance = await SimpleStorage.deployed();
 
     // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    await simpleStorageInstance.set("QmQo6GX8jpHxfJoMomNHnVwGxaEUXx3eRC7jTkPVzX5vwh", { from: accounts[0] });
 
     // Get stored value
     const storedData = await simpleStorageInstance.get.call();
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    assert.equal(storedData, "QmQo6GX8jpHxfJoMomNHnVwGxaEUXx3eRC7jTkPVzX5vwh", "Correct hash was not stored and returned.");
   });
 });
